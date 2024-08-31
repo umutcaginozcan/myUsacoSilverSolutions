e80779718850a5e2471efa74871890ce245c7044
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-08-31 14:09:54 +0300

    Searching: USACO 2013 December P2 (O(N^3) )Solution
    32.1mb
    56ms
    2
    *
    32.1mb
    56ms
    Submitted; Results below show the outcome for each judge test case
    *
    *
    *
    *
    *
    *
    13
    32.4mb
    67ms
    4
    32.4mb
    73ms
    5
    32.6mb
    128ms
    6
    32.6mb
    209ms
    7
    32.7mb
    239ms
    8
    32.6mb
    354ms
    9
    32.6mb
    377ms
    32.6mb
    10
    452ms

---
+++ PS_2_Searching/usaco2013dec.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class usaco2013dec {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("baseball.in")));
+		PrintWriter out = new PrintWriter("baseball.out");
+		
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] cow_locations = new int[N];
+        for (int i = 0; i < N; i++) {
+            cow_locations[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(cow_locations);
+
+        int result = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int dist1 = cow_locations[j] - cow_locations[i];
+                    int dist2 = cow_locations[k] - cow_locations[j];
+                    result += (dist2 >= dist1 && dist2 <= 2 * dist1) ? 1 : 0;
+                }
+            }
+        }
+        out.println(result);
+        out.close();
+    }
+}
