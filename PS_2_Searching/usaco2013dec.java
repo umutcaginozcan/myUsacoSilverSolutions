ff904422162bfdb39504555f41a10bba2b48d9fb
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-08-31 23:26:03 +0300

    Searching: USACO 2013 December P2 Most Optimal Solution

--- PS_2_Searching/usaco2013dec.java
+++ PS_2_Searching/usaco2013dec.java
@@ -20,15 +20,17 @@ public class usaco2013dec {
         Arrays.sort(cow_locations);
 
         int result = 0;
-        for (int i = 0; i < N - 2; i++) {
-            outerloop:
-            for (int j = i + 1; j < N - 1; j++) {
-                for (int k = j + 1 ; k < N; k++) {
-                    int dist1 = cow_locations[j] - cow_locations[i];
-                    int dist2 = cow_locations[k] - cow_locations[j];
-                    result += (dist2 >= dist1 && dist2 <= 2 * dist1) ? 1 : 0;
-                    if (dist2 >= dist1) continue outerloop;
-                }
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                int lower_bound = 2 * cow_locations[j] - cow_locations[i];
+                int higher_bound = 3 * cow_locations[j] - 2 * cow_locations[i];
+                
+                int left_index = Arrays.binarySearch(cow_locations, lower_bound);
+                if (left_index < 0) left_index = Math.abs(left_index + 1);
+                int right_index = Arrays.binarySearch(cow_locations, higher_bound);
+                if (right_index < 0) right_index = Math.abs(right_index + 2);
+
+                result += right_index - left_index + 1;
             }
         }
         out.println(result);
