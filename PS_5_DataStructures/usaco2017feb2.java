b4d377c299a48e1e88f863f2234b4b0de4e40e86
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-09-20 17:07:41 +0300

    fix

--- PS_5_DataStructures/usaco2017feb2.java
+++ PS_5_DataStructures/usaco2017feb2.java
@@ -1,25 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.io.*;
+import java.util.*;
 
 public class usaco2017feb2 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
-
+        BufferedReader br = new BufferedReader(new FileReader("helpcross.in"));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("helpcross.out")));
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
         int C = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-
+ 
         ArrayList<Integer> chickens = new ArrayList<>();
   
         for (int i = 0; i < C; i++) {
@@ -27,36 +17,31 @@ public class usaco2017feb2 {
         }
         Collections.sort(chickens);
         
-        ArrayList<ArrayList<Integer>> cows = new ArrayList<ArrayList<Integer> >();
+        ArrayList<int[]> cows = new ArrayList<>();
         for (int i = 0; i < N; i++) {
-            cows.add(new ArrayList<Integer>());
-
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
-            cows.get(i).add(x1);
-            cows.get(i).add(x2);
+            cows.add(new int[]{x1, x2});
         }
 
-        Collections.sort(cows, (a,  b) -> {
-            if (a.get(1) == b.get(1))
-                return a.get(0) - b.get(0);
-            return a.get(1) - b.get(1);
+        Collections.sort(cows, (a, b) -> {
+            if (a[1] == b[1])
+                return a[0] - b[0];
+            return a[1] - b[1];
         });
         
         int count = 0;
-        int j = 0; // Chickens
+        int j = 0; // Index for chickens
         for (int i = 0; i < N && j < C; i++) {
-            int x1 = cows.get(i).get(0);
-            int x2 = cows.get(i).get(1);
-            int x = chickens.get(j);
+            int x1 = cows.get(i)[0];
+            int x2 = cows.get(i)[1];
             
-            while (j < C && x < x1) {
+            while (j < C && chickens.get(j) < x1) {
                 j++;
-                x = chickens.get(j);
             }
 
-            if (j < C && x <= x2) {
+            if (j < C && chickens.get(j) <= x2) {
                 count++;
                 j++;
             }
