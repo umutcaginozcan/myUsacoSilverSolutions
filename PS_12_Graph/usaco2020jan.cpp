62920b9a971244bff24b6da685fd6a822b6fe848
Umut Çağın Özcan <umutcaginozcan@gmail.com>
2024-11-24 16:43:35 +0300

    Graphs: USACO 2020 January P3 (Full credit)

--- PS_12_Graph/usaco2020jan.cpp
+++ PS_12_Graph/usaco2020jan.cpp
@@ -1,94 +1,101 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-static vector<int> wrongEdges;
-static vector<bool> visited;
-static vector<vector<int>> adj;
-static int wrongCnt;
-static int N, M;
-static int visitedcnt;
+int N, M;
+vector<vector<int>> wormholes;
+vector<int> visited;
+vector<int> cows;
+vector<int> wrong;
 
-void dfs(int curnode) {
-    visited[curnode] = true;
-    visitedcnt++;
-    for (int nextnode : adj[curnode]) {
-        if (!visited[nextnode]) {
-            dfs(nextnode);
+void dfs(int v, vector<vector<int>>& adjlist, vector<int>& visited) {
+    visited[v] = 1;
+    for (int u : adjlist[v]) {
+        if (!visited[u - 1]) {
+            dfs(u - 1, adjlist, visited);
         }
     }
 }
 
-bool valid(vector<vector<int>>& wormholes, int curwormhole) {
-    // Initialize adjacency list and visited array
-    adj.assign(N, vector<int>());
-    visited.assign(N, false);
-    visitedcnt = 0;
-
-    // Build the graph from the current wormhole index
-    for (int i = curwormhole; i < wormholes.size(); i++) {
-        int u = wormholes[i][0];
-        int v = wormholes[i][1];
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+bool valid(int testIndex) {
+    // construct the graph: use adjacency list
+    vector<vector<int>> adjlist(N);
+    for (int i = testIndex; i < M; i++) {
+        int c1 = wormholes[i][0], c2 = wormholes[i][1];
+        adjlist[c1 - 1].push_back(c2);
+        adjlist[c2 - 1].push_back(c1);
     }
 
-    // Perform DFS from each edge if not visited
-    for (int i = 0; i < M; i++) {
-        for (int j = 0; j < adj[i].size(); j++) {
-            if (!visited[adj[i][j]])
-                dfs(adj[i][j]);
+    // Find the first node with edges
+    int start = -1;
+    for (int i = 0; i < N; i++) {
+        if (!adjlist[i].empty()) {
+            start = i;
+            break;
         }
     }
 
-    // Check if all wrong nodes were visited
-    for (int i = 0; i < wrongCnt; i++) {
-        if (!visited[wrongEdges[i]])
+    // Flood fill the adjlist
+    visited.assign(N, 0);
+    if (start != -1) dfs(start, adjlist, visited); // Start DFS only if a valid start exists
+
+    // Check if all nodes marked 'wrong' are visited
+    for (int cow : wrong) {
+        if (!visited[cow - 1])
             return false;
     }
     return true;
 }
 
 int main() {
-    cin >> N >> M;
+    ifstream fin("wormsort.in");
+    ofstream fout("wormsort.out");
 
-    vector<int> cows(N, 0);
-    for (int i = 0; i < N; i++)
-        cin >> cows[i];
+    fin >> N >> M;
+    cows.resize(N);
+    wormholes.resize(M, vector<int>(3));
+    visited.resize(N);
 
-    wrongEdges.clear();
-    wrongCnt = 0;
+    bool alreadySorted = true;
     for (int i = 0; i < N; i++) {
-        if (cows[i] != i) {
-            wrongCnt++;
-            wrongEdges.push_back(i);
+        fin >> cows[i];
+        if (cows[i] != i + 1) {
+            alreadySorted = false;
+            wrong.push_back(cows[i]);
         }
     }
 
-    vector<vector<int>> wormholes(M, vector<int>(3));
-    for (int i = 0; i < M; i++) {
-        cin >> wormholes[i][0];
-        cin >> wormholes[i][1];
-        cin >> wormholes[i][2];
+    if (alreadySorted) {
+        fout << -1;
+        return 0;
     }
 
-    sort(wormholes.begin(), wormholes.end(),
-        [](const vector<int>& w1, const vector<int>& w2) {
-            return w1[2] < w2[2];
-        });
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < 3; j++) {
+            fin >> wormholes[i][j];
+        }
+    }
+
+    sort(wormholes.begin(), wormholes.end(), [](const vector<int> &a, const vector<int> &b) {
+        return a[2] < b[2]; 
+    });
 
     int lo = 0, hi = M - 1, res = -1;
     while (lo <= hi) {
         int mid = (lo + hi) / 2;
-        if (valid(wormholes, mid)) {
-            res = wormholes[mid][2];
-            hi = mid - 1;
-        } else {
+        if (valid(mid)) {
             lo = mid + 1;
+            res = wormholes[mid][2];
+        } else {
+            hi = mid - 1;
         }
     }
 
-    cout << "Result: " << res << endl;
+    fout << res;
+
+    fin.close();
+    fout.close();
     return 0;
 }
